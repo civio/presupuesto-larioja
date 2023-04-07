e6d24566a7e01e6bcb9c233615ff84dc5cff4224
David Cabo <david.cabo@gmail.com>
2023-04-08 01:37:09 +0200

    Create basic loader for 2022 budget, and first-cut categories civio/presupuesto-management#1256

--- loaders/larioja_budget_loader.py
+++ loaders/larioja_budget_loader.py
@@ -1,9 +1,89 @@
 # -*- coding: UTF-8 -*-
-from budget_app.models import *
 from budget_app.loaders import SimpleBudgetLoader
-import re
+
+
+expenses_mapping = {
+    # 'default': {},
+    '2022': { 'ic_code': 1, 'fc_code': 2, 'full_ec_code': 3, 'description': 23, 'forecast_amount': 40, 'actual_amount': 999 },
+}
+
+income_mapping = {
+    # 'default': {},
+    '2022': { 'full_ec_code': 0, 'description': 11, 'forecast_amount': 13, 'actual_amount': 999 },
+}
+
+
+class BudgetCsvMapper:
+    def __init__(self, year, is_expense):
+        column_mapping = income_mapping
+
+        if is_expense:
+            column_mapping = expenses_mapping
+
+        mapping = column_mapping.get(str(year))
+
+        if not mapping:
+            mapping = column_mapping.get('default')
+
+        self.ic_code = mapping.get('ic_code')
+        self.fc_code = mapping.get('fc_code')
+        self.full_ec_code = mapping.get('full_ec_code')
+        self.description = mapping.get('description')
+        self.forecast_amount = mapping.get('forecast_amount')
+        self.actual_amount = mapping.get('actual_amount')
+
 
 class LaRiojaBudgetLoader(SimpleBudgetLoader):
+    # make year data available in the class and call super
+    def load(self, entity, year, path, status):
+        self.year = year
+        SimpleBudgetLoader.load(self, entity, year, path, status)
 
+    # Parse an input line into fields
     def parse_item(self, filename, line):
-        return None
+        # Type of data
+        is_expense = (filename.find('gastos.csv') != -1)
+        is_actual = (filename.find('/ejecucion_') != -1)
+
+        # Mapper
+        mapper = BudgetCsvMapper(self.year, is_expense)
+
+        # Economic code
+        # Concepts are the firts three digits from the economic codes
+        # Item numbers are the last two digits from the economic codes (fourth and fifth digits)
+        full_ec_code = line[mapper.full_ec_code].strip()
+        ec_code = full_ec_code[:3]
+        item_number = full_ec_code[-2:]
+
+        # Description
+        description = line[mapper.description].strip().decode("iso-8859-1")
+
+        # Parse amount
+        amount = line[mapper.actual_amount if is_actual else mapper.forecast_amount]
+        amount = self._parse_amount(amount)
+
+        # Expenses
+        if is_expense:
+            # Institutional code
+            ic_code = line[mapper.ic_code].strip()
+            ic_code = '000' # FIXME
+
+            # Functional code
+            fc_code = line[mapper.fc_code].strip()
+
+        # Income
+        else:
+            # We don't have institutional or functional codes in income
+            fc_code = None
+            ic_code = '000'
+
+        return {
+            'is_expense': is_expense,
+            'is_actual': is_actual,
+            'fc_code': fc_code,
+            'ec_code': ec_code,
+            'ic_code': ic_code,
+            'item_number': item_number,
+            'description': description,
+            'amount': amount
+        }
