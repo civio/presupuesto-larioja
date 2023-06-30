8d45d616e56e368595edd8b172529892a2b5f74a
David Cabo <david.cabo@gmail.com>
2023-06-30 23:26:23 +0200

    Use Plausible for analytics civio/presupuesto-management#1269

--- settings.py
+++ settings.py
@@ -96,9 +96,8 @@ LANGUAGES = (
   # ('gl', 'Galego'),
 )
 
-# Google Analytics ID. Default: ''
-# In order to get the ID create a Google Analytics Acount in https://analytics.google.com/analytics/web/
-ANALYTICS_ID = 'G-5BJMECZ22B'
+# Plausible data domain. Default: ''
+PLAUSIBLE_DOMAIN = 'larioja.dondevanmisimpuestos.es'
 
 # Setup Data Source Budget link
 DATA_SOURCE_BUDGET = 'https://www.larioja.org/hacienda/es/presupuestos'
