2e1e57ab5ff9f3cc3a2ead1f2dd35ce94f4deb35
David Cabo <david.cabo@gmail.com>
2025-03-20 22:21:17 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,1 +1,6 @@
-from larioja_budget_loader import LaRiojaBudgetLoader
+import six
+
+if six.PY2:
+    from larioja_budget_loader import LaRiojaBudgetLoader
+else:
+    from .larioja_budget_loader import LaRiojaBudgetLoader
